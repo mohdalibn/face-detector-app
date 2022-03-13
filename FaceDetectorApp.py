00de54b8140ed4e9e89a63469ea5d84726a40287
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-13 15:50:33 +0300

    Created Canvas and uploaded the correct BG image

--- FaceDetectorApp.py
+++ FaceDetectorApp.py
@@ -22,6 +22,15 @@ FaceDetectionApp.geometry("800x550")  # Size of the tkinter window
 FaceDetectionApp.iconbitmap('AppImages/AppIcon.ico')
 FaceDetectionApp.configure(bg="#fff")
 
+# Creating a Canvas for the App
+canvas = Canvas(FaceDetectionApp, bg="#ffffff", height=550,
+                width=800, bd=0, highlightthickness=0, relief="ridge")
+canvas.place(x=0, y=0)
+
+# Loading the Background Image of the App
+background_img = PhotoImage(file=f"AppImages/background.png")
+background = canvas.create_image(400.0, 275.0, image=background_img)
+
 
 # Starting the Tkinter MainLoop
 FaceDetectionApp.mainloop()
