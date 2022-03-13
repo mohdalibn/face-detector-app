0b780bd6ede2c5fefcf7898db06afcf87ee5353a
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-13 15:35:31 +0300

    Added basic tkinter window code

--- FaceDetectorApp.py
+++ FaceDetectorApp.py
@@ -12,3 +12,14 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import cv2
+
+
+# Initializing Tkinter instanct
+FaceDetectionApp = Tk()
+FaceDetectionApp.title("Face Detection App")
+FaceDetectionApp.geometry("800x550")  # Size of the tkinter window
+FaceDetectionApp.configure(bg="#fff")
+
+
+# Starting the Tkinter MainLoop
+FaceDetectionApp.mainloop()
