8148fc9eccbe12f172630a51ad38499abd8ea43f
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-13 15:39:35 +0300

    Added App icon

--- FaceDetectorApp.py
+++ FaceDetectorApp.py
@@ -18,6 +18,8 @@ import cv2
 FaceDetectionApp = Tk()
 FaceDetectionApp.title("Face Detection App")
 FaceDetectionApp.geometry("800x550")  # Size of the tkinter window
+# Added an Icon for the App
+FaceDetectionApp.iconbitmap('AppImages/AppIcon.ico')
 FaceDetectionApp.configure(bg="#fff")
 
 
